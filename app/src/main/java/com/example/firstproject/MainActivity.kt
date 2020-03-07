e0adb51c69a9806cc3169cce4adc42a2f9929f1e
Vishnu Unnikrishnan <optra7@gmail.com>
2020-03-08 00:28:53 +0530

    pwoli

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.firstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Year
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onSubmit(view:View){
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        hbAge.text = "Age : " + (currentYear - tv.text.toString().toInt() )
+
     }
 }
